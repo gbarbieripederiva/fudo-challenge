7b6c242bb9c9517d4fa6ac1abe573926b8c08004
gbarbieripederiva <gbarbieripederiva@gmail.com>
2021-10-21 16:02:39 -0300

    Now works on windows+static serving

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import path from "path";
+import fs from "fs";
 import { getDB } from "./persistence/database";
 import AuthRoute from "./routes/authRoute";
 import FruitsRoute from "./routes/fruitRoute";
 
 const PORT = 8081;
+const publicFolder = path.join(__dirname,"public");
 const app = express();
 
 getDB().catch((e) => {
@@ -11,9 +14,12 @@ getDB().catch((e) => {
 });
 
 app.use("/", express.json());
-
 app.use("/api/auth", AuthRoute);
 app.use("/api/fruit", FruitsRoute);
+
+if (fs.existsSync(publicFolder)) {
+    app.use("/",express.static(publicFolder))
+}
 
 app.listen(PORT, () => {
     console.log(`API Running on port ${PORT}...`);
